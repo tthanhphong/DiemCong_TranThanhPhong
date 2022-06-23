7fa2269ffece3dc9aa5da5737f9c908ae2806c50
ADMIN <ADMIN@LAPTOP-4A4QRVPB>
2022-06-23 12:51:30 +0700

    Add project files.

---
+++ DiemCong_TranThanhPhong/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DiemCong_TranThanhPhong.MvcApplication" Language="C#" %>
---
+++ DiemCong_TranThanhPhong/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiemCong_TranThanhPhong.Startup))]
+namespace DiemCong_TranThanhPhong
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
